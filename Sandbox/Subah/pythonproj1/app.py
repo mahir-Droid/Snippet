655852fc727fbcf108b5ed0317f228cf37084542
sj neha <subahjareen@gmail.com>
2021-10-24 18:33:14 +0600

    python day 1 basic practice

---
+++ Sandbox/Subah/pythonproj1/app.py
@@ -0,0 +1,29 @@
+# Getting started with python
+print("Subah Jareen")
+print("o----")
+print(" ||||")
+print("*" * 10)
+price = 10  # Price is an integer identifier
+price = 20
+print(price)
+name = "Neha"
+is_done = False
+patient_name = "John Smith"
+patient_age = 20
+is_new_patient = True
+# Receiving input
+# name_input = input("What is your name? ")
+# fav_colour = input("What is your favourite colour? ")
+# print(name_input + " likes " + fav_colour)  # Concatenating Strings
+# birth_year = input("Birth year: ")
+# age = 2021 - int(birth_year)
+# print(age)
+# print(type(age))
+# weight_in_pounds = input("Weight in pounds: ")
+# weight_in_kgs = float(weight_in_pounds) * 0.45
+# print(weight_in_kgs)
+# Use triple quotes to display multiple lines of string
+course = "Python for beginners"
+print(course[0:5])  # Excludes the fifth index
+
+
