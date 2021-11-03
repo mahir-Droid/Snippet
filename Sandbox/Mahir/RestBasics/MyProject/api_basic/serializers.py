86c9d33010800bb00118f604b9607ddf978423a3
Mostafa Mushsharat <mushsharat@gmail.com>
2021-11-03 21:11:47 +0600

    Django REST: Add serializers, functionbased api-views classbased api-views

--- Sandbox/Mahir/RestBasics/MyProject/api_basic/serializers.py
+++ Sandbox/Mahir/RestBasics/MyProject/api_basic/serializers.py
@@ -1,20 +1,10 @@
+from django.db.models import fields
 from rest_framework import serializers
 from .models import Article
 
 
-class ArticleSerializer(serializers.Serializer):
-    title = serializers.CharField(max_length=100)
-    author = serializers.CharField(max_length=100)
-    email = serializers.EmailField(max_length=100)
-    date = serializers.DateTimeField()
-
-    def create(self, validate_data):
-        return Article.objects.create(validate_data)
-
-    def update(self, validate_data):
-        instance.title = validate_data.get('title', instance.title)
-        instance.author = validate_data.get('title', instance.author)
-        instance.email = validate_data.get('title', instance.email)
-        instance.date = validate_data.get('title', instance.date)
-        instance.save()
-        return instance
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Article
+        #fields = ['id', 'title', 'author', 'email']
+        fields = '__all__'
