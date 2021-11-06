13ca0573758c67ed755fe5182eaad3f1ce285e7a
Mostafa Mushsharat <mushsharat@gmail.com>
2021-11-06 22:30:08 +0600

    Django REST: Add viewset, genericviewset, modelviewset, routers, tokenauth, basicauth

--- Sandbox/Mahir/RestBasics/MyProject/api_basic/urls.py
+++ Sandbox/Mahir/RestBasics/MyProject/api_basic/urls.py
@@ -1,9 +1,17 @@
-from django.urls import path
-from .views import ArticleAPIView, ArticleDetails, article_list, article_detail
+from django.urls import path, include
+from .views import ArticleAPIView, ArticleDetails, GenericAPIView, article_list, article_detail, ArticleViewSet
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+router.register('article', ArticleViewSet, basename='article')
 
 urlpatterns = [
    # path('article/', article_list),
    # path('detail/<int:pk>/', article_detail),
     path('article/', ArticleAPIView.as_view()),
     path('detail/<int:id>/', ArticleDetails.as_view()),
+    path('generic/article/<int:id>', GenericAPIView.as_view()),
+    path('viewset/', include(router.urls)),
+    path('viewset/<int:pk>/', include(router.urls))
 ]
--- Sandbox/Mahir/RestBasics/MyProject/api_basic/views.py
+++ Sandbox/Mahir/RestBasics/MyProject/api_basic/views.py
@@ -1,17 +1,95 @@
+from django.contrib import auth
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse, response
+from rest_framework import authentication
+from rest_framework import permissions
 from rest_framework.parsers import JSONParser
 from .models import Article
 from .serializers import ArticleSerializer
 from django.views.decorators.csrf import csrf_exempt
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
 from rest_framework.response import Response
-from rest_framework import status
-
+from rest_framework import serializers, status
 from rest_framework.views import APIView
-
+from rest_framework import generics
+from rest_framework import mixins
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
+
+#model viewset
+class ArticleViewSet(viewsets.ModelViewSet):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+
+"""
+#Generic viewset
+class ArticleViewSet(viewsets.GenericViewSet, mixins.ListModelMixin,  mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+"""
+
+"""
+#viewset
+class ArticleViewSet(viewsets.ViewSet):
+    def list(self, request):
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+    
+    def create(self, request):
+        serializer = ArticleSerializer(data = request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def retrieve(self, request, pk=None):
+        queryset = Article.objects.all()
+        article = get_object_or_404(queryset, pk=pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    def update(self, request, pk=None):
+        article = Article.objects.get(pk=pk)
+        serializer = ArticleSerializer(article, data = request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+"""
+
+
+
+class GenericAPIView(generics.GenericAPIView, mixins.ListModelMixin, mixins.CreateModelMixin, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+    
+    serializer_class = ArticleSerializer
+    queryset = Article.objects.all()
+    #authentication_classes = [SessionAuthentication, BasicAuthentication]
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    lookup_field = 'id'
+
+    def get(self, request, id=None):
+        if id:
+            return self.retrieve(request)
+        else:
+            return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+    def put(self, request, id):
+        return self.update(request, id)
+
+    def delete(self, request, id):
+        return self.destroy(request, id)
 
 #Class Based API views
 class ArticleAPIView(APIView):
