3d13492004a548568b95b42b53b96ae0ef255433
sj neha <subahjareen@gmail.com>
2021-11-08 23:44:55 +0600

    user interface rough outline (tentative)

---
+++ Sandbox/Subah/pythonproj1/practice.py
@@ -0,0 +1,16 @@
+def greet_user(first_name, last_name):
+    print(f'Hi {first_name}{last_name}!')
+    print('Welcome aboard.')
+
+
+print("Start")
+greet_user(first_name="Subah", last_name=" Jareen")
+print("Finish")
+
+
+def square(number):
+    return number * number
+
+
+result = square(3)
+print(result)
