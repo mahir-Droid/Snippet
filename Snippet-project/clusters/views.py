ec7f8e46605933710306f82c3bf8352febdfcd19
Mostafa Mushsharat <mushsharat@gmail.com>
2021-11-23 02:05:30 +0600

    Add Scrapper
    Add short sample scrapping code to test and reuse later

--- Snippet-project/clusters/views.py
+++ Snippet-project/clusters/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
 
+import requests
+from bs4 import BeautifulSoup
+
+r = requests.get("https://www.dsebd.org/")
+soup = BeautifulSoup(r.content, 'html.parser')
+
+headings = soup.find_all()
+
+data = []
+
+for th in headings:
+    data.append(th.text)
+
+
+
+
 
 def home(request):
-    return render(request, 'clusters/home.html')
+    return render(request, 'clusters/home.html', {'data':data})
+    
